de2a5610a240aa66b924930466ebe470aa5d3d3c
Ankit Patel <ap991348@gmail.com>
2024-06-05 21:34:49 +0530

    First commit

---
+++ footer.php
@@ -0,0 +1,14 @@
+		<!-- Footer -->
+				<!-- /Footer -->
+
+		<!-- preloader -->
+		<div id='preloader'><div class='preloader'></div></div>
+		<!-- /preloader -->
+
+
+		<!-- jQuery Plugins -->
+		<script type="text/javascript" src="js/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+
+    </body>
